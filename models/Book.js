1d6f50332f7d49ce17423a40d4bff50743ef3031
afzaaljavaidcoderepo <64521405+afzaaljavaid47@users.noreply.github.com>
2023-12-15 01:26:42 +0500

    Initial commit

---
+++ models/Book.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose');
+
+const BookSchema=new mongoose.Schema({
+    BookISBN:{
+        type:Number,
+        required:true
+    },
+    BookTitle:{
+        type:String,
+        required:true,
+    },
+    BookAuthor:{
+        type:String,
+        required:true,
+    },
+    BookGenre:{
+        type:String,
+        required:true
+    },
+    UserName:{
+        type:String,
+        required:true
+    }
+})
+const book=mongoose.model("Book",BookSchema);
+module.exports=book;
